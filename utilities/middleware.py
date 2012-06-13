8b421ba47cfbb4006b9a44409afa3a1e6d949fd2
Greg Brown <gregplaysguitar@gmail.com>
2012-06-13 12:32:30 +1200

    add/remove slash middleware updated

--- utilities/middleware.py
+++ utilities/middleware.py
@@ -1,19 +1,23 @@
 from django import http
 from django.utils.http import urlquote
 from django.core import urlresolvers
+from django.conf import settings
+
 
 
 class AppendOrRemoveSlashMiddleware(object):
-    """
-    Like django's built in APPEND_SLASH functionality, but also works in reverse. Eg
+    """Like django's built in APPEND_SLASH functionality, but also works in reverse. Eg
     will remove the slash if a slash-appended url won't resolve, but its non-slashed
     counterpart will.
-    """
+    
+    Additionally, if a 404 error is raised within a view for a non-slashed url, and 
+    APPEND_SLASH is True, and the slash-appended url resolves, the middleware will
+    redirect. (The default APPEND_SLASH behaviour only catches Resolver404, so
+    wouldn't work in this case.)"""
 
     def process_request(self, request):
-        """
-        Returns a redirect if adding/removing a slash is appropriate. 
-        """
+        """Returns a redirect if adding/removing a slash is appropriate. This works
+        in the same way as the default APPEND_SLASH behaviour but in either direction."""
         
         # check if the url is valid
         urlconf = getattr(request, 'urlconf', None)
@@ -26,16 +30,31 @@ class AppendOrRemoveSlashMiddleware(object):
             
             if _is_valid_path(new_path, urlconf):
                 # if the new url is valid, redirect to it
-                if request.get_host():
-                    new_url = "%s://%s%s" % (request.is_secure() and 'https' or 'http', request.get_host(), urlquote(new_path))
-                else:
-                    new_url = urlquote(new_path)
-                if request.GET:
-                    new_url += '?' + request.META['QUERY_STRING']
-                return http.HttpResponsePermanentRedirect(new_url)
-
-            
+                return http.HttpResponsePermanentRedirect(generate_url(request, new_path))
+    
+    def process_response(self, request, response):
+        """If a 404 is raised within a view, try appending a slash and redirecting.
+        Note we can't also go back the other way, because this might cause an
+        infinite loop."""
         
+        if response.status_code == 404 and not request.path_info.endswith('/') and settings.APPEND_SLASH:
+            new_path = request.path_info + '/'
+            urlconf = getattr(request, 'urlconf', None)
+            if _is_valid_path(new_path, urlconf):
+                return http.HttpResponsePermanentRedirect(generate_url(request, new_path))
+        return response
+            
+
+def generate_url(request, path):
+    if request.get_host():
+        new_url = "%s://%s%s" % (request.is_secure() and 'https' or 'http', request.get_host(), urlquote(path))
+    else:
+        new_url = urlquote(path)
+    if request.GET:
+        new_url += '?' + request.META['QUERY_STRING']
+    return new_url
+
+
 def _is_valid_path(path, urlconf=None):
     """
     Returns True if the given path resolves against the default URL resolver,
