c536a62a2db51b4861172ec2d76e673e40bdae7a
Greg Brown <gregplaysguitar@gmail.com>
2012-08-03 16:44:21 +1200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
-import cms
+import utilities
 from setuptools import setup, find_packages
 from setuptools.command.test import test
 
 setup(
-    name='djangocms2000',
-    version=cms.__version__,
+    name='django-utilities',
+    version=utilities.__version__,
     description='My own collection of django utilities',
     long_description=open('readme.markdown').read(),
     author='Greg Brown',
     author_email='greg@gregbrown.co.nz',
-    url='https://github.com/gregplaysguitar/djangocms2000',
+    url='https://github.com/gregplaysguitar/django-utilities',
     packages=find_packages(exclude=['tests', 'tests.*']),
     platforms='any',
     zip_safe=False,
